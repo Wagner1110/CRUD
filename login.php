0bb1dc13c5c6a5145fee54678383ef86e4d3a0f0
Wagnersolano <wagnersolano536@gmail.com>
2022-07-25 15:43:48 -0600

    login

--- login.php
+++ login.php
@@ -1,29 +1,29 @@
 <?php
-session_start();
-if(isset($_SESSION['datos'])){
- header('location:index.html');   
-}else{
-    $dbhost="localhost";
-    $dbuser="CRUD";
-    $dbpass="";
-    $dbname="";
-    $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-    if(!$conn){
-        die("Error en la conexion a base de datos: " .mysqli_connect_error());
+include("conexion.php");
+$con = conectar();
+
+if(isset($_POST['correo'])&& isset($_POST['pass'])){
+
+    $correo= $_POST['correo'];
+    $pass = $_POST['pass'];
+   
+
+    $sql = mysqli_query($con,"SELECT * FROM usuarios WHERE correo = '". $correo."'AND pass='".$pass."'");
+    $cr = mysqli_num_rows($sql);
+    if($cr==1){
+        header('location:index.php');
+    }else{
+        echo"El usuario no existe";
+        header('location:login.html');
     }
-    if(isset($_POST['correo'])&& isset($_POST['pass'])){
-        $correo = $_POST['correo'];
-        $pass = $_POST['pass'];
+}
 
-        $sql = mysqli_query($conn,"SELECT * FROM crud WHERE correo ='".$correo . "' AND pass='". $pass ."'");
-$cr = mysqli_num_rows($sql);
-if(!isset($_SESSION['datos'])){
-    $_SESSION['datos'] = $correo;
-    header('location:index.php');
-}else{
-    header('location:login.html');
-} 
 
-    }
 
-} 
+
+
+
+
+
+
+?>
